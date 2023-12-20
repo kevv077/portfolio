25f232d53847c9ecbab2a521d9b2f9e34410c329
Kevin Amaral <147442592+kevv077@users.noreply.github.com>
2023-12-19 22:33:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Holaa, quiero aclarar que solamente se HTML y CSS, puse el resto de skills de ejemplo, y es mi objetivos aprender esos lenguajes, gracias <3 ");
+//alert("Holaa, quiero aclarar que solamente se HTML y CSS, puse el resto de skills de ejemplo, y es mi objetivos aprender esos lenguajes, gracias <3 ");
